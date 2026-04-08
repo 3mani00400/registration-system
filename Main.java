9d1555ad5b02ef0b0a3f841c31c9495d616f69b2
3mani00400 <ST10499759@rcconnect.edu.za>
2026-04-08 08:29:16 +0200

    Added while conditions for the user's preference/ choice

--- Main.java
+++ Main.java
@@ -1,3 +1,14 @@
-public class Main {
-    
-}
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Registration {
+    public static void main(String[] args) {
+        HashMap<String, String> userDatabase = new HashMap<>();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printIn("Welcome!");
+        
+        while (true) {
+            
+        }
+    }
